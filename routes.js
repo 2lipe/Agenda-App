11da1bc12323f8c8bdf5cf789a684e4e7a958b3f
Felipe <felipe.svfx@gmail.com>
2020-07-31 20:07:49 -0300

    add rotas de contato

--- routes.js
+++ routes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const homeController = require('./src/controllers/homeController');
 const registerController = require('./src/controllers/registerController');
 const loginController = require('./src/controllers/loginController');
+const loggedController = require('./src/controllers/loggedController');
+const contatoController = require('./src/controllers/contatoController');
+const contatoFormController = require('./src/controllers/contato-formController');
+
+const { loginRequired } = require('./src/middlewares/middlewares');
 
 const route = express.Router();
 
@@ -16,5 +21,11 @@ route.post('/register/register', registerController.register);
 route.get('/login', loginController.index);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
+
+// Rotas de contato
+route.get('/logged', loginRequired, loggedController.index);
+route.get('/contato', loginRequired, contatoController.index);
+route.get('/form-contato', loginRequired, contatoFormController.index);
+route.post('/form-contato/register', loginRequired, contatoFormController.register);
 
 module.exports = route;
